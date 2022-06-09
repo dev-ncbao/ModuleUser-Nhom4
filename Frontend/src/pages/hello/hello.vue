8d33ea0d409e51be4277d277c2b6273e69b1aaff
Bao <ncbao.dev@gmail.com>
2022-06-09 16:51:12 +0700

    Add Call Api Exam

--- Frontend/src/pages/hello/hello.vue
+++ Frontend/src/pages/hello/hello.vue
@@ -1,4 +1,13 @@
 <template src="./hello.html"></template>
 <style src="./hello.scss" lang="scss"></style>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    fetch('http://localhost:5555/users', {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+        method: 'GET',
+        credentials: 'include',
+    }).then((res) => console.log(res))
+    //.then((data) => console.log(data))
+</script>
