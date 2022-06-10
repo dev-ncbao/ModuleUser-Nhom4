a10d65ad9f46425664b714593ad2dee82ca67bdf
NguyenDu2710 <nguyendu123876@gmail.com>
2022-06-10 08:55:17 +0700

    ok

---
+++ Backend/ModuleUser/Data/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModuleUser.Entities;
+
+namespace ModuleUser.Data
+{
+    public class UserDbContext: DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options): base (options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
